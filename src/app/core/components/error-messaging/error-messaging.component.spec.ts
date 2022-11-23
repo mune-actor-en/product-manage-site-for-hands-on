215e61fd7f11750e7a307401aa1992da8a50e78e
m-hiroiwa <masamune.hiroiwa@fusionia.net>
2022-11-23 12:11:26 +0900

    セクション3:サインイン作成-42.ユニットテストまで修了

---
+++ src/app/core/components/error-messaging/error-messaging.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ErrorMessagingComponent } from './error-messaging.component';
+
+xdescribe('ErrorMessagingComponent', () => {
+  let component: ErrorMessagingComponent;
+  let fixture: ComponentFixture<ErrorMessagingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ErrorMessagingComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ErrorMessagingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
