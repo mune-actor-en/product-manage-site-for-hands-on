8d8d047dadb2131592e1f813bd98c9173480ce2c
m-hiroiwa <masamune.hiroiwa@fusionia.net>
2022-12-30 14:40:20 +0900

    first commit

--- src/app/shared/services/title-i18.service.spec.ts
+++ src/app/shared/services/title-i18.service.spec.ts
@@ -1,16 +1,38 @@
+import { TranslateTestingModule } from 'ngx-translate-testing';
+
 import { TestBed } from '@angular/core/testing';
 
 import { TitleI18Service } from './title-i18.service';
 
-xdescribe('TitleI18Service', () => {
+describe('TitleI18Service', () => {
   let service: TitleI18Service;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        TranslateTestingModule.withTranslations({
+          ja: require('src/assets/i18n/ja.json'),
+        }),
+      ],
+    });
     service = TestBed.inject(TitleI18Service);
   });
+  describe('#constractor', () => {
+    it('should be created', () => {
+      expect(service).toBeTruthy();
+    });
+  });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('#setTitle', () => {
+    it('should set title', () => {
+      const screenName = 'sign-in';
+      const expectedTitleSystem = '【Example Site】';
+      const expectedTitleSub = 'ログイン';
+
+      service.setTitle(screenName);
+      expect(service.title.getTitle()).toEqual(
+        expectedTitleSystem + expectedTitleSub
+      );
+    });
   });
 });
